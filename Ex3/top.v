cc47510b5c7836d4798755adb61f74003641ae93
jl-wang9 <jl-wang9@users.noreply.github.com>
2021-06-15 10:21:49 +0000

    Completed code, but to check

--- Ex3/top.v
+++ Ex3/top.v
@@ -18,14 +18,13 @@
 
 `timescale 1ns / 100ps
 
-module monitor (
+module monitor (rst, clk, change, on_off, counter_out);
     //Todo: add ports 
-    input rst,			// reset
-    input clk,			// clock
-    input change,		// indicator if device is on or off
-    input on_off,		// type of event
-    output reg[7:0]counter_out	// 8-bit output register of time
-    );
+    input rst;			// reset
+    input clk;			// clock
+    input change;		// indicator if device is on or off
+    input on_off;		// type of event
+    output reg[7:0]counter_out;	// 8-bit output register of time
                     
     //Todo: add registers and wires, if needed
     // Not needed as reg assigned in output above.
@@ -34,11 +33,12 @@ module monitor (
     always @(posedge clk or posedge rst)	// Sensitivity list that is true when rising edge for clk
     begin
 	if(rst)
-	   counter_out <= 0;			// Counter set to zero when reset is triggered
+	   counter_out <= 8'd0;			// Counter set to zero when reset is triggered
 	else
 	   begin
 	   if(change)				// When change==1
-		counter_out = on_off? counter_out + 1 : counter_out - 1;
+		counter_out = on_off? (counter_out + 8'd1) : (counter_out - 8'd1);
+		
 	   else
 		counter_out = counter_out;		// Counter const if change==0
 	   end
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -16,14 +16,15 @@ module top_tb(
 //Todo: Parameters
 parameter CLK_PERIOD = 10;	// Clock period
 
-//Todo: Regitsers and wires
-reg err;
-reg rst;
-reg clk;
-reg change;
-reg on_off;
-reg counter_old;
-wire counter_out;
+
+//Todo: Initialise Registers and wires
+reg err = 1'b0;    // Error flag
+reg rst_tb;
+reg clk_tb = 1'b0;
+reg change_tb;
+reg on_off_tb;
+reg [7:0]counter_old = 8'd0;     // For tracking previous value
+wire [7:0]counter_out_tb;
 
 
 //Todo: Clock generation
@@ -31,80 +32,110 @@ wire counter_out;
 
 initial
 begin
-   clk = 0;
    forever
-	#(CLK_PERIOD/2)clk =~ clk;	// Happens every delay of 5 time units
+	#(CLK_PERIOD/2)clk_tb =~ clk_tb;	// Happens every delay of 5 time units
 end
 
 
 //Todo: User logic
-
 // Check reset feature
 initial
 begin
-   // Initial inputs to test
-   rst = 1;
-   change = 0;
-   on_off = 0;
-   err = 0;			// Error flag 
-   
-   forever begin
-	#(CLK_PERIOD)
-
-	if(counter_out != 8'b0)		// triggered if counter not zero (not supposed to happen)
- 	begin
-	   $display("***TEST FAILED, counter not reset when reset = 1***");
-	   err = 1;
-   	end
-   end
+    forever begin
+        rst_tb = 1'b1;
+        change_tb = 1'b0;
+        on_off_tb = 1'b0;
+        #(CLK_PERIOD)
+        
+        if(counter_out_tb != 8'd0)	    // triggered if counter not zero (not supposed to happen)
+        begin
+            if(err != 1'b1)    // Added so error message only prints once
+            begin
+                $display("***TEST FAILED, counter not reset when reset = 1***");
+                err = 1'b1;
+            end
+        end
+        
+        
+        //  PASSED RESET TEST
+        // Check change feature
+        else
+        begin
+            // Set reset to 0 & get initial counter value
+            rst_tb <= 1'b0;
+            counter_old <= counter_out_tb;
+            #(CLK_PERIOD)
+            
+            if(counter_old != counter_out_tb)		// triggered if counter not zero (not supposed to happen)
+            begin
+                if(err != 1'b1)         // Added so error message only prints once
+                begin
+                    $display("***TEST FAILED, counter NOT CONSTANT when change is 0***");
+                    err = 1'b1;
+                end
+            end
+            
+            
+            // PASSED CHANGE TEST
+            // Check counting features
+            else
+            begin
+                change_tb <= 1'b1;
+                counter_old <= counter_out_tb;
+                #(CLK_PERIOD)
+                
+                // Check if successfully counted down
+                if((counter_old - counter_out_tb) != 8'd1)
+                begin
+                    if(err != 1'b1)         // Added so error message only prints once
+                    begin
+                        $display("***TEST FAILED, counter did not count DOWN when required!***");
+                        err = 1'b1;
+                    end
+                end
+                
+                
+                // Check if successfully counted up
+                else
+                begin
+                    on_off_tb <= 1'b1;
+                    counter_old <= counter_out_tb;
+                    #(CLK_PERIOD)
+                    
+                    // Check if successfully counted up
+                    if((counter_out_tb - counter_old) != 8'd1)
+                    begin
+                        if(err != 1'b1)         // Added so error message only prints once
+                        begin
+                            $display("***TEST FAILED, counter did not count UP when required!***");
+                            err = 1'b1;
+                        end
+                    end
+                end
+            end
+        end            
+    end
 end
-
-
-
-// Check change feature
-initial
-begin
-   // Initial inputs to test
-   counter_old = 0;
-   rst = 0;
-   change = 0;
-   on_off = 0;
-   err = 0;			// Error flag 
-
-   forever begin
-	#(CLK_PERIOD)
-
-	if(counter_old != counter_out)		// triggered if counter not zero (not supposed to happen)
- 	begin
-	   $display("***TEST FAILED, counter NOT CONSTANT when change is 0***");
-	   err = 1;
-   	end
-   end
-end
-
-
-
 
 
     
 //Todo: Finish test, check for success
 initial
 begin
-   //#(256*CLK_PERIOD)		// Run for max possible period for a 8-bit to form 
-   #5120
-   if (err==0)
-	$display("***TEST PASSED! :) ***");
+   #500 
+   if (err!=1'b1)
+      $display("***TEST PASSED! :) ***");
    $finish;
 end
 
 
 //Todo: Instantiate counter module
     monitor top (
-     .rst (rst),
-     .clk (clk),
-     .change (change),
-     .on_off (on_off),
-     .counter_out (counter_out)
+     .rst (rst_tb),
+     .clk (clk_tb),
+     .change (change_tb),
+     .on_off (on_off_tb),
+     .counter_out (counter_out_tb)
      );
  
 endmodule 
